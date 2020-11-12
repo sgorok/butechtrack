95280a8018a068a518112bbdbf6fed8506e2065d
Daniel Shimon <danielshimon@Daniels-MacBook-Pro.local>
2020-11-12 17:52:15 -0500

    added a new page for contact

--- app.py
+++ app.py
@@ -3,11 +3,15 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-@app.route("/home")
-
+#@app.route("/home")
 def home():
     html_template = 'site.html'
     return render_template(html_template)
+
+@app.route("/contact")
+def contact():
+  return render_template("contact.html")
+
 
 if __name__ == "__main__":
     app.run()
